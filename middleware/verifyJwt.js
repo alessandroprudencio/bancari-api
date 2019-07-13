f0c4600bf05914aeb87d7451ee63afcd4a5f0ca6
alessandroprudencio <alessandroconectado@gmail.com>
2019-07-13 18:12:11 -0400

    install nodemon

--- middleware/verifyJwt.js
+++ middleware/verifyJwt.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken'
 
 const verifyToken = async (req, res, next) => {
-    if (!req.headers.authorization) return res.status(401).send({ message: 'Token não encontrado' })
+    if (!req.headers.authorization) return res.status(401).send({ message: 'Token não encontrado, por favor faça login !' })
     if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
         jwt.verify(req.headers.authorization.split(" ")[1], process.env.SECRET_TOKEN, function (err, jwt) {
             if (err) return res.status(401).send({ message: err.message })
