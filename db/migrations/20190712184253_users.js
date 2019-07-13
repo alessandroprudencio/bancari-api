9ba7088f52be17e8a388ac5bed7832a03b963e09
alessandroprudencio <alessandroconectado@gmail.com>
2019-07-13 17:21:54 -0400

    install nodemon

--- db/migrations/20190712184253_users.js
+++ db/migrations/20190712184253_users.js
@@ -2,9 +2,9 @@
 exports.up = function (knex) {
   return knex.schema.createTable('users', table => {
     table.increments('id').primary()
-    table.string('name')
+    table.string('name').notNullable();
     table.string('email').unique().notNullable();
-    table.string('password')
+    table.string('password').notNullable();
     table.string('image')
     table.boolean('admin').defaultTo(false)
     table.timestamp('created_at').defaultTo(knex.fn.now());
--- db/migrations/20190713165209_residents.js
+++ db/migrations/20190713165209_residents.js
@@ -2,12 +2,12 @@
 exports.up = function (knex) {
     return knex.schema.createTable('residents', table => {
         table.increments('id').primary()
-        table.string('name')
+        table.string('name').notNullable();
         table.string('email').unique().notNullable();
         table.string('image')
-        table.text('address')
-        table.string('phone')
-        table.integer('number_address')
+        table.text('address').notNullable();
+        table.string('phone').notNullable();
+        table.integer('number_address').notNullable();
         table.timestamp('created_at').defaultTo(knex.fn.now());
     })
 };
