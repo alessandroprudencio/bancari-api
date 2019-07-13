c57c5e2f26ee6e9d6a96477dc043b31f74d3c3e9
alessandroprudencio <alessandroconectado@gmail.com>
2019-07-13 16:12:26 -0400

    install nodemon

--- db/migrations/20190712184253_users.js
+++ db/migrations/20190712184253_users.js
@@ -7,7 +7,7 @@ exports.up = function (knex) {
       table.string('password')
       table.string('image')
       table.boolean('admin').defaultTo(false)
-      table.timestamps(true, true)
+      table.timestamps(true)
     })
   };
   
