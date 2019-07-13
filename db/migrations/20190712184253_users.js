83637c819e4a09feed8d74009488eefffb7bb210
alessandroprudencio <alessandroconectado@gmail.com>
2019-07-13 17:10:11 -0400

    install nodemon

--- db/migrations/20190712184253_users.js
+++ db/migrations/20190712184253_users.js
@@ -1,17 +1,16 @@
 // const uuidv4 = require('uuid/v4')
 exports.up = function (knex) {
-    return knex.schema.createTable('users', table => {
-      table.increments('id').primary()
-      table.string('name')
-      table.string('email').unique().notNullable();
-      table.string('password')
-      table.string('image')
-      table.boolean('admin').defaultTo(false)
-      table.timestamp('created_at').defaultTo(knex.fn.now());
-    })
-  };
-  
-  exports.down = function (knex) {
-    return knex.schema.dropTable('users')
-  };
-  
+  return knex.schema.createTable('users', table => {
+    table.increments('id').primary()
+    table.string('name')
+    table.string('email').unique().notNullable();
+    table.string('password')
+    table.string('image')
+    table.boolean('admin').defaultTo(false)
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+  })
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable('users')
+};
--- db/migrations/20190713165209_residents.js
+++ db/migrations/20190713165209_residents.js
@@ -1,5 +1,5 @@
 
-exports.up = function(knex) {
+exports.up = function (knex) {
     return knex.schema.createTable('residents', table => {
         table.increments('id').primary()
         table.string('name')
@@ -9,9 +9,9 @@ exports.up = function(knex) {
         table.string('phone')
         table.integer('number_address')
         table.timestamp('created_at').defaultTo(knex.fn.now());
-      })
+    })
 };
 
-exports.down = function(knex) {
+exports.down = function (knex) {
     return knex.schema.dropTable('residents')
 };
