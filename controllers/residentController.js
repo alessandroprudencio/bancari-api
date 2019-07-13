dacc654a6f73b5a8603bf71a857868545cc1ec69
alessandroprudencio <alessandroconectado@gmail.com>
2019-07-13 17:27:18 -0400

    install nodemon

--- controllers/residentController.js
+++ controllers/residentController.js
@@ -4,7 +4,7 @@ const knex = require('knex')(config)
 import validator from 'validator'
 
 const getResident = (req, res) => {
-    knex.select('name', 'email', 'endereco', 'telefone', 'numero').from('residents').then(data => {
+    knex.select('name', 'email', 'address', 'phone', 'number_address').from('residents').then(data => {
         res.send(data)
     });
 }
