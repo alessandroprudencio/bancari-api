f64953d24cc977ebaa4aaac369277d36e65c8c0c
alessandroprudencio <alessandroconectado@gmail.com>
2019-07-13 17:06:56 -0400

    install nodemon

--- controllers/residentController.js
+++ controllers/residentController.js
@@ -4,21 +4,20 @@ const knex = require('knex')(config)
 import validator from 'validator'
 
 const getResident = (req, res) => {
-    knex.select('name', 'email', 'endereco', 'telefone', 'numero').from('Residents').then(data => {
+    knex.select('name', 'email', 'endereco', 'telefone', 'numero').from('residents').then(data => {
         res.send(data)
     });
 }
 
 const createResident = async (req, res) => {
-    const { name, email, endereco, telefone, image } = req.body
+    const { name, email, address, phone, number_address } = req.body
 
-    if (!name || !email || !password || !confirmPassword) return res.status(400).send({ message: 'Por favor preencha todos os campos' })
+    if (!name || !email || !address || !phone || !number_address) return res.status(400).send({ message: 'Por favor preencha todos os campos' })
 
     if (!validator.isEmail(email)) return res.status(400).send({ message: 'E-mail inválido' })
-
     
 
-    knex('Residents').insert(req.body)
+    knex('residents').insert(req.body)
         .then(() => {
             res.json({ message: "Morador cadastrado com sucesso!" })
         }).catch(error => {
@@ -27,18 +26,15 @@ const createResident = async (req, res) => {
 }
 
 const updateResident = (req, res) => {
-    const { password } = req.body
 
-    if (req.body.password) req.body.password = bcrypt.hashSync(password, 10)
-
-    knex('Residents').update(req.body)
+    knex('residents').update(req.body)
         .then(() => res.send({ message: "Atualizado com sucesso!" }))
         .catch(err => res.send({ message: err.sqlMessage }))
 }
 
 const deleteResident = (req, res) => {
 
-    knex.select('id').from('Residents').where({ id: req.params.id })
+    knex.select('id').from('residents').where({ id: req.params.id })
         .then(Resident => {
             if (!Resident.length) res.send({ message: "Usuário não encontrado" })
 
