611c18155dde98bfb7f983e5c3ed0c41e07b9129
alessandroprudencio <alessandroconectado@gmail.com>
2019-07-14 00:16:09 -0400

    install nodemon

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -3,8 +3,6 @@ import { login } from '../controllers/authController'
 const router = require('express').Router()
 
 router
-    .post('/', (req,res)=>{
-        res.send('hahsdkjhsakjhdjk')
-    })
+    .post('/', login)
 
 export default router
