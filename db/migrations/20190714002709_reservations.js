b72cd67751a2c8bf4e2a807cfb7f45fc71aac02b
alessandroprudencio <alessandroconectado@gmail.com>
2019-07-14 01:37:16 -0400

    install nodemon

--- db/migrations/20190714002709_reservations.js
+++ db/migrations/20190714002709_reservations.js
@@ -2,7 +2,7 @@
 exports.up = function(knex) {
     return knex.schema.createTable('reservations', table => {
         table.increments('id').primary()
-        table.foreign('id','resident_id').onDelete("CASCADE").unsigned()
+        table.foreign('id','resident_id').onDelete("CASCADE")
         table.string('local').notNullable();
         table.datetime('date').notNullable()
         table.timestamp('created_at').defaultTo(knex.fn.now());
