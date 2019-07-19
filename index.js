af0714e27090358bcd40058dc269e47b6c7b811e
Alessandro <alessandroconectado@gmail.com>
2019-07-19 14:28:48 -0400

    Inplemetando api

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use('/login', authRoute)
 import userRoute from './routes/userRoute'
 app.use('/user', userRoute)
 
-app.use('/file', express.static(path.resolve(__dirname, '.', 'uploads', 'profile')))
+app.use('/file', express.static(path.resolve(__dirname, '.')))
 
 import residentRoute from './routes/residentRoute'
 app.use('/resident', verifyJwt, residentRoute)
