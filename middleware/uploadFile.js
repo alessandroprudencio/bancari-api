23466600e5e1131b74845a6299fa8fe59895e220
Alessandro <alessandroconectado@gmail.com>
2019-07-20 17:10:52 -0400

    agora vai

--- middleware/uploadFile.js
+++ middleware/uploadFile.js
@@ -1,15 +1,12 @@
 const upload = async (req, res) => {
     const { name, size, mimetype, md5 } = req.files.image
     var allowedMimes = ['image/jpeg', 'image/pjpeg', 'image/png'];
-    if (!allowedMimes.includes(mimetype)) return sendStatus(400).send({ message: "Extensão de foto não permitida!" })
-    if (size > 5 * (1024 * 1024)) return sendStatus(400).send('Tamanho de imagem não permitido')
+    if (!allowedMimes.includes(mimetype)) return status(400).send({ message: "Extensão de foto não permitida!" })
+    if (size > 5 * (1024 * 1024)) return status(400).send('Tamanho de imagem não permitido')
     const nameFile = md5 + (path.extname(name))
-    try {
-        await req.files.image.mv(__dirname + '../../uploads/profile/' + nameFile)
+    await req.files.image.mv(__dirname + '../../uploads/profile/' + nameFile,(err)=>{
+        if(err)return status(500).send(err)
         return nameFile
-    } catch (err) {
-        console.log(err)
-        sendStatus(500).send(err)
-    }
+    })
 }
 export default upload
