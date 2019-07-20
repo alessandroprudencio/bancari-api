42203a39a9e93f03f585629f2138dbf767d91d7f
Alessandro <alessandroconectado@gmail.com>
2019-07-20 10:34:03 -0400

    Agora vai

--- controllers/authController.js
+++ controllers/authController.js
@@ -8,14 +8,14 @@ import validator from 'validator'
 const login = async (req, res) => {
     const { email, password } = req.body
 
-    if (!email || !password) res.status(400).send({ message: 'Passe os dados e não me fo!@#!' })
+    if (!email || !password) return res.status(400).send({ message: 'Passe os dados e não me fo!@#!' })
 
     if (!validator.isEmail(email)) return res.status(400).send({ message: 'E-mail inválido' })
 
     let user = await knex('users').select('email', 'password', 'admin').whereRaw('email = ?', email)
-    if (!user.length) res.status(404).send({ message: 'E-mail não encontrado' })
+    if (!user.length) return res.status(404).send({ message: 'E-mail não encontrado' })
 
-    if (bcrypt.compareSync(req.body.password, user[0].password)) res.status(200).send({ email: email, token: jwt.sign({ user: req.body, admin: user[0].admin }, process.env.SECRET_TOKEN, { expiresIn: '1h' }) })
+    if (bcrypt.compareSync(req.body.password, user[0].password)) return res.status(200).send({ email: email, token: jwt.sign({ user: req.body, admin: user[0].admin }, process.env.SECRET_TOKEN, { expiresIn: '1h' }) })
     else res.status(401).send({ message: 'Dados inválidos' })
 
 }
