3e9cf06cca9ec3a97a08daf17fad706dddd667fc
Alessandro <alessandroconectado@gmail.com>
2019-07-19 21:35:19 -0400

    Bug]

--- controllers/realtyController.js
+++ controllers/realtyController.js
@@ -2,7 +2,7 @@ const knex = require('knex')(require('../db/knexfile')[process.env.NODE_ENV || '
 
 const getRealty = async (req, res) => {
     try {
-        res.send(await knex('realtys').join('residents', 'realtys.owner_id', 'residents.id').select('residents.name', 'realtys.address', 'realtys.number_address'))
+        res.send(await knex('realtys').leftJoin('residents', 'realtys.owner_id', 'residents.id').select('residents.name', 'realtys.address', 'realtys.number_address'))
     } catch (error) {
         res.status(500).send({ message: error })
     }
@@ -24,7 +24,7 @@ const createRealty = async (req, res) => {
 
     try {
         await knex('realtys').insert(req.body)
-        res.json({ message: "Reserva efetuada com sucesso!" })
+        res.json({ message: "Imóvel cadastrado com sucesso!" })
     } catch (error) {
         res.status(500).send({ message: error })
     }
