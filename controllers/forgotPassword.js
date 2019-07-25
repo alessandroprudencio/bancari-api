e38a4cca43ff7adb6afd3c5faa3f98aa07a351a1
Alessandro <alessandroconectado@gmail.com>
2019-07-25 10:50:57 -0400

    Agora vai

--- controllers/forgotPassword.js
+++ controllers/forgotPassword.js
@@ -38,18 +38,6 @@ const forgotPassword = async (req, res) => {
              res.send(`OK! E-mail para recuperação de senha enviado para o email  ${email}`)
         }
       });
-      return
-
-    if (!email || !password) return res.status(400).send({ message: 'Passe os dados e não me fo!@#!' })
-
-    if (!validator.isEmail(email)) return res.status(400).send({ message: 'E-mail inválido' })
-
-    let user = await knex('users').select('email', 'password', 'admin').whereRaw('email = ?', email)
-    if (!user.length) return res.status(404).send({ message: 'E-mail não encontrado' })
-
-    if (bcrypt.compareSync(req.body.password, user[0].password)) return res.status(200).send({ email: email, token: jwt.sign({ user: req.body, admin: user[0].admin }, process.env.SECRET_TOKEN, { expiresIn: '1h' }) })
-    else res.status(401).send({ message: 'Dados inválidos' })
-
 }
 
 export default forgotPassword
