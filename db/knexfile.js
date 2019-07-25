8c099ab6560edd70870db7545d65681ff2784c7b
Alessandro <alessandroconectado@gmail.com>
2019-07-25 09:15:31 -0400

    Agora vai

--- db/knexfile.js
+++ db/knexfile.js
@@ -7,9 +7,25 @@ module.exports = {
       host: '127.0.0.1',
       user: 'postgres',
       password: 'root',
-      database: 'bancari',
-      timezone: "UTC"  
+      database: 'bancari',  
     },
+    pool: {
+      afterCreate: function (conn, done) {
+        // in this example we use pg driver's connection API
+        conn.query('SET timezone="UTC-3";', function (err) {
+          if (err) {
+            // first query failed, return error and don't try to make next query
+            done(err, conn);
+          } else {
+            // do the second query...
+            conn.query('SELECT set_limit(0.01);', function (err) {
+              // if err is not falsy, connection is discarded from pool
+              // if connection aquire was triggered by a query the error is passed to query promise
+              done(err, conn);
+            });
+          }
+        });
+      },
     migrations: {
       directory: __dirname + '/migrations',
     },
@@ -48,9 +64,25 @@ module.exports = {
       host: 'ec2-174-129-29-101.compute-1.amazonaws.com',
       database: 'd69bc587hlphpp',
       user: 'guscabrjqkgezj',
-      password: '2860cbf4f11497d69e011e4c37e9fb6432c64ff295ffb593307a69434e7c273b',
-      timezone: "UTC"      
+      password: '2860cbf4f11497d69e011e4c37e9fb6432c64ff295ffb593307a69434e7c273b',      
     },
+    pool: {
+      afterCreate: function (conn, done) {
+        // in this example we use pg driver's connection API
+        conn.query('SET timezone="UTC-3";', function (err) {
+          if (err) {
+            // first query failed, return error and don't try to make next query
+            done(err, conn);
+          } else {
+            // do the second query...
+            conn.query('SELECT set_limit(0.01);', function (err) {
+              // if err is not falsy, connection is discarded from pool
+              // if connection aquire was triggered by a query the error is passed to query promise
+              done(err, conn);
+            });
+          }
+        });
+      },
     migrations: {
       directory: __dirname + '/migrations',
     }
