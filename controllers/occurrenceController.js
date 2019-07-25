2caea008828e6dd2d4de7864413ecb0a0c255fdb
Alessandro <alessandroconectado@gmail.com>
2019-07-25 16:23:45 -0400

    Agora vai

--- controllers/occurrenceController.js
+++ controllers/occurrenceController.js
@@ -1,6 +1,6 @@
 const getOccurrence = async (req, res) => {
     try {
-        res.send(await knex('occurrences').join('users', 'occurrences.user_id', 'users.id').select('occurrences.id','users.name as user_name','occurrences.message')).orderBy('occurrences.update_at')
+        res.send(await knex('occurrences').join('users', 'occurrences.user_id', 'users.id').select('occurrences.id','occurrences.update_at','users.name as user_name','occurrences.message')).orderBy('occurrences.update_at')
     } catch (err) {
         res.status(500).send({ message: err })
     }
