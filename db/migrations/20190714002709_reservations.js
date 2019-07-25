fa667a76096691a0896abe8f310dd1e9c8fe8e8f
Alessandro <alessandroconectado@gmail.com>
2019-07-25 09:58:05 -0400

    Agora vai

--- db/migrations/20190714002709_reservations.js
+++ db/migrations/20190714002709_reservations.js
@@ -1,6 +1,5 @@
 exports.up = async  knex => {
     await knex.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
-    await knex.raw('SET timezone TO "America/Sao_Paulo"')
     return knex.schema.createTable('reservations', table => {
         table.string('id', 50).unique().notNullable().primary().defaultTo(knex.raw('uuid_generate_v4()'))
         table.string('resident_id', 100).references("id").inTable("residents").index().onDelete("CASCADE")
