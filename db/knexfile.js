b687feaa6277a30a33dd1fa9d37591164b167ee6
Alessandro <alessandroconectado@gmail.com>
2019-07-25 10:13:26 -0400

    Agora vai

--- db/knexfile.js
+++ db/knexfile.js
@@ -51,7 +51,7 @@ module.exports = {
     },
     pool: {
       afterCreate: function(connection, callback) {
-        connection.query('SET timezone = brst;', function(err) {
+        connection.query('SET timezone = "America/Sao_Paulo"', function(err) {
           callback(err, connection);
         });
       }
