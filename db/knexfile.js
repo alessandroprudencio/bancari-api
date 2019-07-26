5f61b125c0f547f1a00ea657713102739a262c03
Alessandro <alessandroconectado@gmail.com>
2019-07-25 22:52:33 -0400

    Mudando knexfile

--- db/knexfile.js
+++ db/knexfile.js
@@ -1,59 +1,59 @@
 
 module.exports = {
-  
-    development: {
-  client: 'pg',
-  connection: {
-    host: '127.0.0.1',
-    user: 'postgres',
-    password: '',
-    database: 'bancari',
+
+  development: {
+    client: 'pg',
+    connection: {
+      host: '127.0.0.1',
+      user: 'postgres',
+      password: '',
+      database: 'bancari',
+    },
+    migrations: {
+      directory: __dirname + '/migrations',
+    },
   },
-  migrations: {
-    directory: __dirname + '/migrations',
-  },
-},
-//   development: {
-//   client: 'mysql',
-//   connection: {
-//     host: '127.0.0.1',
-//     user: 'root',
-//     password: '',
-//     database: 'bancari',
-//   },
-//   migrations: {
-//     directory: __dirname + '/migrations',
-//   },
-// },
+  //   development: {
+  //   client: 'mysql',
+  //   connection: {
+  //     host: '127.0.0.1',
+  //     user: 'root',
+  //     password: '',
+  //     database: 'bancari',
+  //   },
+  //   migrations: {
+  //     directory: __dirname + '/migrations',
+  //   },
+  // },
 
-//   production: {
-//   client: 'mysql',
-//   connection: {
-//     host: 'us-cdbr-iron-east-02.cleardb.net',
-//     database: 'heroku_7bc1b53f7100315',
-//     user: 'badc4d7ee11162',
-//     password: 'eec6ebc1'
-//   },
-//   migrations: {
-//     directory: __dirname + '/migrations',
-//   },
-// }
+  //   production: {
+  //   client: 'mysql',
+  //   connection: {
+  //     host: 'us-cdbr-iron-east-02.cleardb.net',
+  //     database: 'heroku_7bc1b53f7100315',
+  //     user: 'badc4d7ee11162',
+  //     password: 'eec6ebc1'
+  //   },
+  //   migrations: {
+  //     directory: __dirname + '/migrations',
+  //   },
+  // }
 
 
 
-  production:{
+  production: {
     client: 'pg',
     connection: {
       host: 'bancari.c9tznxtz79mr.us-east-1.rds.amazonaws.com',
       database: 'bancari',
       user: 'postgres',
-      port:5432,
+      port: 5432,
       password: 'QDJwdIhN8cC1MOsDLHv7'
     },
     migrations: {
       directory: __dirname + '/migrations',
     }
   }
-  
+
 }
 
